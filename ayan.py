812fb455a98f355955de5e4557e80dd22e2c677f
abhiqty-ctrl <abhibrahman1221@gmail.com>
2026-07-08 17:25:43 +0530

    Update ayan.py

--- ayan.py
+++ ayan.py
@@ -74,13 +74,7 @@ RAID_TEXTS = [
 "a guy gave me wise advice "ignore kro jaise mai krta hu coz vo randike bachhe hai" so imma apply that on you.",
 "🪄 अंतर मंत्र शैतानी खोपड़ा तेरी मां का भोसड़ा 🪄",
 "𝐓ᴜ]ʜ𝐄 𝐂ᴏᴏ𝐋 𝐁ᴏʟ𝐔 𝐘ᴀ 𝐑ɴΔ𝐈 𝐊ᴀ 🇧聲🇨🇨ʜA 🔥😸🔥😸🔥😸🔥😸🔥",
-"teri ma ko चोद kr chath se fek diya 
-
-                            🤸🏻
-                               🤸🏻
-                                    🤸🏻
-                                         🤸🏻 
-                                             👩🏻‍🦽👩🏻‍🦽👩🏻‍🦽",
+"teri ma ko चोद dunga",
 "🏃🏻🏃🏻🏃🏻🏃🏻 teri maa chodke bhaaga abusive",
 "Teri ma ki चुत me 🥚 dalke fry krdunga 🤙🏻",
 "tri ma rndy",
